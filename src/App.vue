5a87e1364db2ec971deb7177b09218a3e7098b56
giospes <giovannijacopo.spes@gmail.com>
2023-04-13 14:42:09 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="">
+    
+    
+
+  </div>
+</template>
+
+<script>
+  name: 'App'
+  export default {
+    
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,16 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+// import { library } from '@fortawesome/fontawesome-svg-core'
+// import { fas } from '@fortawesome/free-solid-svg-icons'
+// import { fab } from '@fortawesome/free-brands-svg-icons'
+// import { } from '@fortawesome/free-regular-svg-icons'
+// import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+import './assets/main.scss'
+
+// library.add(fas, far, fab)
+
+// .component('fa', FontAwesomeIcon)
+
+createApp(App).mount('#app')
